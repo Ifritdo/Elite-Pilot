3764dfee1127e0342fce0567dbf97be0233ab223
Ifritdo <JulianFirpo316@hotmail.com>
2023-10-07 20:08:57 -0300

    Configuro MainMenu
    Agrego la función de los botones con el Game Object MenuController

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Esta función se encargará de iniciar el juego
+    public void StartGame()
     {
-        
+        // Asume que tu escena de juego se llama "GameScene". Cambia el nombre si es necesario.
+        SceneManager.LoadScene("GameScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    // Esta función se encargará de cerrar el juego
+    public void ExitGame()
     {
-        
+        // Cierra la aplicación
+        Application.Quit();
+
+        // Esta línea es solo para que funcione el botón de salir en el Editor de Unity.
+        // Puedes comentarla o eliminarla cuando vayas a compilar tu juego.
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    // Si decides tener un botón que regrese al menú desde la pantalla de GameOver, puedes usar esta función
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
